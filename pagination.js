36c46a700eee0d66b6b4b88c35142ab76949485f
Azzan Braxton <azzanbraxton@gmail.com>
2017-04-17 22:37:48 +0800

    Pagination Final
    Final update, hopefully…..

--- pagination.js
+++ pagination.js
@@ -1,25 +1,23 @@
 //Dynamically add pagination html with .append()
 $('.page').append('<div class="pagination"><ul></ul></div>');
-$('.page-header').append('<div class="student-search"></div>');
 
-//Also, dynamically add list elements
-//Is there a better way of doing this? I feel I skirted around the rules with the dynamic <li>s, but I couldn't think of another way. 
-const _1stList = '<li><a href="#" class="active">1</a></li>';
-const _2ndList = '<li><a href="#">2</a></li>';
-const _3rdList = '<li><a href="#">3</a></li>';
-const _4thList = '<li><a href="#">4</a></li>';
-const _5thList = '<li><a href="#">5</a></li>';
-const _6thList = '<li><a href="#">6</a></li>';
 
-//Place dynamic list in an array and append to ".pagination"
-const pages = [_1stList, _2ndList, _3rdList, _4thList, _5thList, _6thList];
-$(pages).each(function(){
-	$('.pagination ul').append(this);
+//Calculate number of students per page and number of pages as a whole number
+let studentNum = $('.student-item').length;
+let divide = (studentNum / 10);
+let numOfPages = Math.ceil(divide);
+
+//Have the number of buttons automatically calculated based on number of students using a loop
+$(function(){
+	for(i = 1; i <= numOfPages; i++){
+		$('.pagination ul').append('<li><a href="#">' + i + '</a></li>');
+	}
 });
 
 //When the page starts, only show the first 10 students
 $('.page').ready(function(){
 	$('.student-item:gt(9)').hide();
+	$('li a').first().addClass("active");
 });
 
 //When clicking on a different button, remove "active" class from page 1 and stick to page the user clicks
@@ -27,17 +25,13 @@ $(function(){
 	$('li a').click(function(){
 		$('a.active').removeClass('active');
 		$(this).addClass('active');
-		});
-	});
 
-//Use .show() and .hide() to show 10 students for each page
-//Here as well: is there a better method? I searched all around the jQuery API Doc and googled my ass off, but this is what I went with. 
-$('.pagination li a').on('click', function(){
-	if($(this).text() === "1"){
+//Under the same "click, use .show() and .hide() to show 10 students for each page
+	if($('a.active').text() === "1"){
 		$('.student-item:lt(10)').show();
 		$('.student-item:gt(9)').hide();
 	}
-	if($(this).text() === "2"){
+	if($('a.active').text() === "2"){
 		$('.student-item:lt(10)').hide();
 		$('.student-item:gt(9)').show();
 		$('.student-item:gt(19)').hide();
@@ -62,28 +56,13 @@ $('.pagination li a').on('click', function(){
 		$('.student-item:gt(49)').show();
 		$('.student-item:gt(59)').hide();
 	}
+	if($(this).text() === "7"){
+		$('.student-item:lt(60)').hide();
+		$('.student-item:gt(59)').show();
+		$('.student-item:gt(69)').hide();
+	}
+	});
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
